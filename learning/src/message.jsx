6876fd0ba8f0076f99dce368fc3cfb6ab40720d3
hoppercraft <09official123@gmail.com>
2026-05-02 18:38:09 +0545

    react revision

---
+++ learning/src/message.jsx
@@ -0,0 +1,10 @@
+import {useState} from "react";
+
+function Message(){
+    const [name, setname] = useState("");
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message
